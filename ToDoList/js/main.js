ba22a0dec9a6d1b947f0363f44c314b8bb8ae7ea
aviareps_pwolf <pwolf@aviareps.com>
2023-08-08 12:12:23 +0200

    to do list part 2

--- ToDoList/js/main.js
+++ ToDoList/js/main.js
@@ -2,21 +2,45 @@
 /** call the relevant elements to be able to work in JS */
 const inputField = document.getElementById("inputField");
 const addButton = document.getElementById("addButton");
+const listAnchor = document.getElementById("listOfToDos");
+
+/** Store Definition */
+const listOfToDos = [] //stores all todos
+let newToDo = "" // stores the new input value
 
 
 /** create functions for input and button */
 /**INPUT FIELD function */
 const handleNewToDo = (event)=>{
-    console.log(event.target.value);
+    newToDo = event.target.value;
 }
 
 /** BUTTON function */
-const addNewTodDo =()=>{
-    console.log("Button clicked")
+const addNewToDo =()=>{
+    /**adding new item to array */
+    listAnchor.innerHTML = "" //cleans the innerhtml content to zero <li>
+    
+    /** new item adding and render all <li> to frontend */
+    listOfToDos.push(newToDo);
+
+    /**read all items from the array */
+    listOfToDos.map((toDo)=>{
+
+        const li = document.createElement("li");
+        li.innerText = toDo;
+
+        listAnchor.append(li)
+        
+        inputField.value = "";
+    })
+
+
+
+
 }   
 
-
+console.log("list of todos", listOfToDos)
 
 /** add EVENTS to elements */
 inputField.addEventListener("change", handleNewToDo);
-addButton.addEventListener("click", addNewTodDo)
+addButton.addEventListener("click", addNewToDo)
